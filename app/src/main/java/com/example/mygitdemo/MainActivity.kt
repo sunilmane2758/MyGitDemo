93efb4a9e78409e63f43481a030a1ae69140c774
Admin <sunil.mane@risertechub.in>
2023-10-12 13:40:56 +0530

    log added from M_Branch

--- app/src/main/java/com/example/mygitdemo/MainActivity.kt
+++ app/src/main/java/com/example/mygitdemo/MainActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-    private val TAG = "MainActivity111"
+    private val TAG = "MainActivity1234"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
